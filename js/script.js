9188520280ad6c6eaea71380c3ce216ffb4becc0
cibs84 <d.tilesi@gmail.com>
2022-06-16 18:18:15 +0200

    setTimeout() completed

--- js/script.js
+++ js/script.js
@@ -4,28 +4,41 @@
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 
-// 1) Genero in un array 5 numeri casuali invocando una funzione apposita
-// - Creazione array randomNumbers
-// - Con una funzione creo in un loop dei numeri random 
-// - Per ogni numero SE non è già presente nell'array ce lo pusho 
-// - Continuo FINCHE' non arrivo a 5 elementi nell'array
+// 1) Con una funzione creo in un nuovo array randomNumbers 5 numeri random senza doppioni 
+let randomNumbers = genRndNumbersArray(5, 1, 100);
+console.log(randomNumbers);
 
-// 2) Creo un alert in cui si visualizzeranno per 30 secondi i numeri casuali
-// - Creo una funzione setTimeout con un tempo d'attesa di 30000 ms e richiamo in una funzione anonima un alert
-// - Inserisco nell'alert l'array con i 5 numeri generati casualmente
 
-// 3) Chiedo all'utente per 5 volte di seguito di inserire in un prompt i numeri che ha visualizzato precedentemente
+// 2) Creo un alert dove stampo l'array con i 5 numeri generati casualmente
+alert(`Prova a ricordare questi numeri: ${randomNumbers}`);
+
+
+// 3) Dopo 30 secondi dalla chiusura dell'alert, chiedo all'utente per 5 volte di seguito di inserire in un prompt i numeri che ha visualizzato precedentemente
 // - Creo un array userNumbers
-// - In un loop chiedo all'utente per 5 volte di inserire un numero provando a trovare corrispondenza con quelli visualizzati nel precedente alert
-// Per ogni numero:
-// - - Lo pusho nell'array userNumbers
-
-// 4) In un alert si visualizzeranno quanti e quali numeri sono stati individuati
-// - Creo un array rightNumbers
-// - Con un ciclo for scorro i numeri inseriti dall'utente -> array userNumbers
-// Per ogni numero:
-// - - SE è incluso nell'array randomNumbers ALLORA lo pusho nell'array righNumbers
-// - Creo un alert in cui stamperò quanti (rightNumbers.length) e quali (rightNumbers) numeri sono stati individuati
+let userNumbers = [];
+// - Creo un setTimeout che richiama dopo 30sec una funzione anonima che comprende i seguenti passaggi:
+setTimeout(function() {
+    // - In un loop chiedo all'utente per 5 volte di inserire un numero provando a trovare corrispondenza con quelli visualizzati nel precedente alert
+    // Per ogni numero:
+    for (let i = 1; i <= 5; i++) {
+        // - - Pusho nell'array userNumbers
+        userNumbers.push(parseInt(prompt(`Inserisci un numero provando a trovare corrispondenza con quelli visualizzati nel precedente messaggio - ${i}/5`)));
+    }
+    // 4) In un alert si visualizzeranno quanti e quali numeri sono stati individuati
+    // - Creo un array rightNumbers
+    let rightNumbers = [];
+    // - Con un ciclo for scorro i numeri inseriti dall'utente -> array userNumbers
+    // Per ogni numero:
+    for (let i = 0; i < userNumbers.length; i++) {
+        const thisUserNumber = userNumbers[i];
+        // - - SE è incluso nell'array randomNumbers ALLORA lo pusho nell'array righNumbers
+        if (randomNumbers.includes(thisUserNumber)) {
+            rightNumbers.push(thisUserNumber);
+        }
+    }
+    // - Creo un alert in cui stamperò quanti (rightNumbers.length) e quali (rightNumbers) numeri sono stati individuati
+    alert(`Ti sei ricordato ${rightNumbers.length} numeri: ${rightNumbers}`);
+}, 30000);
 
 
 // -----------------------
